e789359dd10dc9b4823a37101c9debcef5960808
Kamil Handzlik <kamol0209@gmail.com>
2024-11-27 11:44:45 +0100

    added and configurated a_users, static templates

--- a_core/a_core/settings.py
+++ a_core/a_core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'a_home',
+    'a_users',
 ]
 
 MIDDLEWARE = [
@@ -120,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
