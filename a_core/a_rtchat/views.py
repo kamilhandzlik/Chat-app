569b85b8293128e7d74aee93a4f9516912e874f2
kamol0209 <kamol0209@gmail.com>
2025-01-08 12:36:20 +0100

    debbuged private chat

--- a_core/a_rtchat/views.py
+++ a_core/a_rtchat/views.py
@@ -5,8 +5,8 @@ from .forms import *
 from .models import *
 
 @login_required
-def chat_view(request, chatroom_name='public-chat'):
-    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+def chat_view(request, chatroom='public-chat'):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom)
     chat_messages = chat_group.chat_messages.all()[:30]   # [:30] ustala liczbę wyświetlonych wiadomości na ostatnie 30 usuń/zmień jeśli chcesz mieć ich więcej/mniej
     form = ChatmessageCreateForm()
 
@@ -36,7 +36,7 @@ def chat_view(request, chatroom_name='public-chat'):
         'chat_messages': chat_messages,
         'form': form,
         'other_user': other_user,
-        'chatroom_name': chatroom_name,
+        'chatroom_name': chatroom,
     }
 
 
@@ -46,22 +46,44 @@ def chat_view(request, chatroom_name='public-chat'):
 
 @login_required
 def get_or_create_chatroom(request, username):
+    # if request.user.username == username:
+    #     return redirect('profile', username=username)
+
+#     other_user = User.objects.get(username=username)
+#     my_chatrooms = request.user.chat_groups.filter(is_private=True)
+
+#     if my_chatrooms.exists():
+#         for chatroom in my_chatrooms:
+#             if other_user in chatroom.members.all():
+#                 chatroom = chatroom
+#                 break
+#             else:
+#                 chatroom = ChatGroup.objects.create(is_private=True)
+#                 chatroom.members.add(other_user, request.user)
+#     else:
+#         chatroom = ChatGroup.objects.create(is_private=True)
+#         chatroom.members.add(other_user, request.user)
+
+#         return redirect('chatroom', chatroom.group_name)
     if request.user.username == username:
         return redirect('profile', username=username)
 
-    other_user = User.objects.get(username=username)
-    my_chatrooms = request.user.chat_groups.filter(is_private=True)
+    other_user = get_object_or_404(User, username=username)
 
-    if my_chatrooms.exists():
-        for chatroom in my_chatrooms:
-            if other_user in chatroom.members.all():
-                chatroom = chatroom
-                break
-            else:
-                chatroom = ChatGroup.objects.create(is_private=True)
-                chatroom.members.add(other_user, request.user)
-    else:
-        chatroom = ChatGroup.objects.create(is_private=True)
-        chatroom.members.add(other_user, request.user)
 
-        return redirect('chatroom', chatroom.group_name)
+    existing_chatroom = ChatGroup.objects.filter(
+        is_private=True,
+        members=request.user
+    ).filter(members=other_user).first()
+
+    if existing_chatroom:
+        return redirect('chatroom', chatroom=existing_chatroom.group_name)  # Używamy 'chatroom' zamiast 'chatroom_name'
+
+
+    new_chatroom = ChatGroup.objects.create(
+        is_private=True,
+        group_name=f"chat_{request.user.username}_{other_user.username}"
+    )
+    new_chatroom.members.add(request.user, other_user)
+
+    return redirect('chatroom', chatroom=new_chatroom.group_name)
