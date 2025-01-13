56256a812447bec9749b4a14c3c6242ce780f845
kamol0209 <kamol0209@gmail.com>
2025-01-13 10:16:31 +0100

    add debugging for chat admin

--- a_core/a_rtchat/models.py
+++ a_core/a_rtchat/models.py
@@ -6,23 +6,33 @@ import shortuuid
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
     groupchat_name = models.CharField(max_length=128, null=True, blank=True)
-    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
-    members = models.ManyToManyField(User, related_name='chat_groups', blank=True)
+    users_online = models.ManyToManyField(
+        User, related_name="online_in_groups", blank=True
+    )
+    members = models.ManyToManyField(User, related_name="chat_groups", blank=True)
     is_private = models.BooleanField(default=False)
-
+    admin = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="admin_groups",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.group_name
 
 
 class GroupMessage(models.Model):
-    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    group = models.ForeignKey(
+        ChatGroup, related_name="chat_messages", on_delete=models.CASCADE
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.author.username} : {self.body}'
+        return f"{self.author.username} : {self.body}"
 
     class Meta:
-        ordering = ['-created']
+        ordering = ["-created"]
--- a_core/a_rtchat/views.py
+++ a_core/a_rtchat/views.py
@@ -7,10 +7,11 @@ from .models import *
 
 
 @login_required
-def chat_view(request, chatroom='public-chat'):
+def chat_view(request, chatroom="public-chat"):
     chat_group = get_object_or_404(ChatGroup, group_name=chatroom)
     chat_messages = chat_group.chat_messages.all()[
-                    :30]  # [:30] ustala liczbę wyświetlonych wiadomości na ostatnie 30 usuń/zmień jeśli chcesz mieć ich więcej/mniej
+        :30
+    ]  # [:30] ustala liczbę wyświetlonych wiadomości na ostatnie 30 usuń/zmień jeśli chcesz mieć ich więcej/mniej
     form = ChatmessageCreateForm()
 
     other_user = None
@@ -27,7 +28,9 @@ def chat_view(request, chatroom='public-chat'):
             if request.user.emailaddress_set.filter(verified=True).exists():
                 chat_group.members.add(request.user)
             else:
-                messages.warning(request, 'You need to verify your email to join the chat!')
+                messages.warning(
+                    request, "You need to verify your email to join the chat!"
+                )
                 return redirect("profile-settings")
 
     if request.htmx:
@@ -37,67 +40,66 @@ def chat_view(request, chatroom='public-chat'):
             message.author = request.user
             message.group = chat_group
             message.save()
-            context = {
-                'message': message,
-                'user': request.user
-            }
-            return render(request, 'partials/chat_message_p.html', context)
+            context = {"message": message, "user": request.user}
+            return render(request, "partials/chat_message_p.html", context)
 
     context = {
-        'chat_messages': chat_messages,
-        'form': form,
-        'other_user': other_user,
-        'chatroom_name': chatroom,
-        'chat_group': chat_group
+        "chat_messages": chat_messages,
+        "form": form,
+        "other_user": other_user,
+        "chatroom_name": chatroom,
+        "chat_group": chat_group,
     }
 
-    return render(request, 'chat.html', context)
+    return render(request, "chat.html", context)
 
 
 @login_required
 def get_or_create_chatroom(request, username):
     if request.user.username == username:
-        return redirect('profile', username=username)
+        return redirect("profile", username=username)
 
     other_user = get_object_or_404(User, username=username)
 
-    existing_chatroom = ChatGroup.objects.filter(
-        is_private=True,
-        members=request.user
-    ).filter(members=other_user).first()
+    existing_chatroom = (
+        ChatGroup.objects.filter(is_private=True, members=request.user)
+        .filter(members=other_user)
+        .first()
+    )
 
     if existing_chatroom:
-        return redirect('chatroom', chatroom=existing_chatroom.group_name)  # Używamy 'chatroom' zamiast 'chatroom_name'
+        return redirect(
+            "chatroom", chatroom=existing_chatroom.group_name
+        )  # Używamy 'chatroom' zamiast 'chatroom_name'
 
     new_chatroom = ChatGroup.objects.create(
         is_private=True,
-        group_name=f"chat_{request.user.username}_{other_user.username}"
+        group_name=f"chat_{request.user.username}_{other_user.username}",
     )
     new_chatroom.members.add(request.user, other_user)
 
-    return redirect('chatroom', chatroom=new_chatroom.group_name)
+    return redirect("chatroom", chatroom=new_chatroom.group_name)
 
 
 @login_required
 def create_groupchat(request):
     form = NewGroupForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = NewGroupForm(request.POST)
         if form.is_valid():
             new_groupchat = form.save(commit=False)
-            new_groupchat.admin = request.user
+            new_groupchat.admin = request.user  # Przypisz admina
             new_groupchat.save()
-            new_groupchat.members.add(request.user)
-            return redirect('chatroom', new_groupchat.group_name)
+            new_groupchat.members.add(request.user)  # Dodaj admina jako członka
+            return redirect("chatroom", new_groupchat.groupchat_name)
 
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'create_groupchat.html', context)
-
+    return render(request, "create_groupchat.html", context)
 
 
 @login_required()
 def chatroom_edit_view(request, chatroom_name):
-    return render(request, 'chatroom_edit.html')
+    return render(request, "chatroom_edit.html")
